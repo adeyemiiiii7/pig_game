3d2886b7164b26ae8a0b5d81a18811606fe5e4dd
adeyemiiiii7 <aladesuyiadeyemi05@gmail.com>
2024-01-22 13:26:17 +0100

    adding dice rolling functions

--- script.js
+++ script.js
@@ -12,3 +12,13 @@ const btnHold = document.querySelector('.btn--hold');
 score0EL.textContent = 0;
 score1El.textContent = 0;
 diceEL.classList.add('hidden')
+
+//Rolling dice functionality
+btnRoll.addEventListener('click', function(){
+    // 1.Generating a random dice roll
+    const dice = Math.trunc(Math.random() * 6) + 1;
+
+    //2 displaying the dice
+    diceEL.classList.remove('hidden');
+    diceEL.src = `dice-${dice}.png`;
+})
